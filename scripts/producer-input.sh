1cea00912a94a7c70bd888d743c9628efa81fde5
Buldozzer <denis@cloudintellects.com>
2019-04-14 13:12:14 +0300

    Updated version

--- scripts/producer-input.sh
+++ scripts/producer-input.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
 export JAVA_HOME="/usr/local/jdk1.8.0_131"
-/home/vagrant/kafka/bin/kafka-topics.sh --zookeeper kafka-lab-1:2181 --replication-factor 1 --partitions 3 --topic input --create
-/home/vagrant/kafka/bin/kafka-topics.sh --zookeeper kafka-lab-1:2181 --replication-factor 1 --partitions 3 --topic output --create
 
 while true; do
 	echo `date -u +"%s"` | /home/vagrant/kafka/bin/kafka-console-producer.sh --topic "input" --broker-list "kafka-lab-1:9092"
