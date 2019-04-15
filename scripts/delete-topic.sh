d8aee2f892e2f830fd1ce755d944e806db76171d
den m <denis@cloudintellects.com>
2019-04-15 20:31:34 +0300

    The final commit #2

--- scripts/delete-topic.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-if [ $# -gt 0 ]; then
-    /home/vagrant/kafka/bin/kafka-topics.sh --delete --zookeeper kafka-lab-1:2181 --topic $1
-else
-    echo "Usage: "$(basename $0)" <topic_name>"
-fi
--- scripts/list-topics.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/home/vagrant/kafka/bin/kafka-topics.sh --zookeeper kafka-lab-1:2181 --list
